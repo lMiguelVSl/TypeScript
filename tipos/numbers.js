d09d9a02d8e1cb8c17043355a92d6b04ae2e10ed
Miguel <sanchez.vc@hotmail.com>
2022-07-05 13:52:01 -0500

    handling numbers and his value

--- tipos/numbers.js
+++ tipos/numbers.js
@@ -1,4 +1,14 @@
 "use strict";
 (() => {
-    let avengers;
+    let avengers = 10;
+    const villains = 20;
+    if (avengers < villains) {
+        console.log('Estamos en problenas');
+    }
+    else {
+        console.log('Nos salvamos');
+    }
+    avengers = 123;
+    avengers = Number('6');
+    console.log({ avengers });
 })();
--- tipos/numbers.ts
+++ tipos/numbers.ts
@@ -1,6 +1,17 @@
 
 (() => {
-    let avengers;
+    let avengers : number = 10;
 
-    
+    const villains : number = 20;
+
+    if (avengers < villains) {
+        console.log('Estamos en problenas');
+    } else {
+        console.log('Nos salvamos');
+    }
+
+    avengers = 123;
+    avengers = Number('6');
+    console.log({avengers});
+
 }) ()
