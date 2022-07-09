47713d9a73427ac704d7254193946f2f95073d97
Miguel <sanchez.vc@hotmail.com>
2022-07-09 10:16:21 -0500

    Arrays

---
+++ tipos/arrays.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    const numbers = [1, 2, 3, 4, 'miguel', true, 7, 8];
+    console.log(numbers);
+    const number = [1];
+    console.log(number);
+    const villains = ['voldemort', 'Miguelinho', 'Loki'];
+    villains.forEach(v => console.log(v.toUpperCase()));
+})();
---
+++ tipos/arrays.ts
@@ -0,0 +1,13 @@
+
+(()=>{
+
+    const numbers : ( string | number | boolean )[] = [1,2,3,4,'miguel',true,7,8];
+    console.log(numbers);
+    
+    const number : number[] = [1];
+    console.log(number);
+
+    const villains : string[] = ['voldemort','Miguelinho', 'Loki'];
+    villains.forEach( v => console.log(v.toUpperCase()));
+    
+}) ()
